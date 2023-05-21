19aee6e7c54d3915d70d397b87cf5e87bc96266d
Yasvanth <130092764+yasvanthkohli@users.noreply.github.com>
2023-05-21 14:44:59 +0530

    Update ownersdb.sql

--- ownersdb.sql
+++ ownersdb.sql
@@ -33,3 +33,33 @@ COPY pets
 FROM 'C:\Users\admin\Desktop\Sql Project\Pets.csv'
 DELIMITER ',' 
 CSV HEADER
+
+/* Create the procudure details table using the available features of the CREATE TABLE
+ statement */
+ 
+CREATE TABLE proceduredetails (
+    proceduretype varchar,
+    proceduresubcode varchar,
+    description varchar,
+    price float
+);
+
+COPY proceduredetails
+FROM 'C:\Users\admin\Desktop\Sql Project\ProceduresDetails.csv'
+DELIMITER ',' 
+CSV HEADER
+
+/* Create the procudure history table using the available features of the CREATE TABLE
+ statement */
+ 
+CREATE TABLE procedurehistory (
+    petid varchar,
+    proceduredate date,
+    proceduretype varchar,
+    proceduresubcode varchar
+);
+
+COPY procedurehistory 
+FROM 'C:\Users\admin\Desktop\Sql Project\ProceduresHistory.csv'
+DELIMITER ',' 
+CSV HEADER;
