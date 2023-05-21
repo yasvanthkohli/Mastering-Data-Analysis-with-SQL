244e517861c5c930a752041df87057c912dd682e
Yasvanth <130092764+yasvanthkohli@users.noreply.github.com>
2023-05-21 14:36:38 +0530

    Add files via upload

---
+++ ownersdb.sql
@@ -0,0 +1,13 @@
+/* Create the owners table using the available features of the CREATE TABLE
+ statement */
+ 
+CREATE TABLE owners (
+    ownerid varchar,
+    name varchar,
+    surname varchar,
+    streetaddress varchar,
+    city varchar,
+    state varchar(2),
+    statefull varchar,
+    zipcode varchar
+);
