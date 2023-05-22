057041d66c76adbab228ef708558ab31ec066fbb
Yasvanth <130092764+yasvanthkohli@users.noreply.github.com>
2023-05-22 12:22:20 +0530

    Delete createtableprocedurehistory.sql

--- createtableprocedurehistory.sql
+++
@@ -1,9 +0,0 @@
-/* Create the procudure history table using the available features of the CREATE TABLE
- statement */
- 
-CREATE TABLE procedurehistory (
-    petid varchar,
-    proceduredate date,
-    proceduretype varchar,
-    proceduresubcode varchar
-);
