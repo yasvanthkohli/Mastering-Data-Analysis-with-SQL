fd781649157fe92bd8775e2829f4d2483530e044
Yasvanth <130092764+yasvanthkohli@users.noreply.github.com>
2023-05-22 12:22:06 +0530

    Delete createtableprocuredetails.sql

--- createtableprocuredetails.sql
+++
@@ -1,9 +0,0 @@
-/* Create the procudure details table using the available features of the CREATE TABLE
- statement */
- 
-CREATE TABLE proceduredetails (
-    proceduretype varchar,
-    proceduresubcode varchar,
-    description varchar,
-    price float
-);
